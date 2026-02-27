e5dbf4d1e4137a0c552b2dde36c3bf22725bc729
UnderOneBreath <memories53@mail.ru>
2026-02-27 12:35:30 +0300

    done

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ api = Api(app)
 with open('awards.json', encoding='utf-8') as f:
     awards = json.load(f)
 
-# TODO: Добавить код для чтения лауреатов из файла
+with open('laureats.json', encoding='utf-8') as f:
+    laureats = json.load(f)
 
 
 @app.route("/api/v1/awards/")
@@ -39,6 +40,31 @@ def award_object(pk):
 
 
 # TODO: Добавить код для получения списка лауреатов
+@app.route("/api/v2/laureats/")
+def laureats_list():
+    try:
+        p = int(request.args.get('p', 0))
+        if p < 0:
+            raise ValueError
+    except ValueError:
+        return abort(400)
+    page = laureats[p * 50:(p + 1)*50]
+    return jsonify({
+        'page': p,
+        'count_on_page': PAGE_SIZE,
+        'total': len(laureats),
+        'items': page,
+    })
 
 
 # TODO: Добавить код для получения лауреата по индексу
+@app.route("/api/v2/laureat/<int:id>/")
+def laureats_index(id):
+    if 0 <= id < len(laureats):
+        return jsonify(laureats[id])
+    else:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
